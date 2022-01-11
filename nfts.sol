54398cdad1370e5af660c56e372d2bb5471ec87e
CriPt0Dev <97566470+CriPt0Dev@users.noreply.github.com>
2022-01-11 20:53:14 -0300

    Create nfts.sol

---
+++ nfts.sol
@@ -0,0 +1,29 @@
+// contracts/NFT.sol
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFT is ERC721URIStorage { // cria novos tokens
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds; // valor incrementado de 0 a 1 2 3 .....
+    address contractAddress; // endereço no mercado
+
+    constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment(); // incrementa o id
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true); // aprovação para trançasão
+        return newItemId; // retorna o id para interagir com o cliente
+    }
+}
